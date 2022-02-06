4ca5aee6106e10e11fedabf93b6e8034e3c0afdd
Rebekah <rbk1008@Gmail.com>
2022-02-05 23:01:10 -0700

    commit tree 7fc0b60331e6d55f6f281c2aa785f5d79750e6e1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,7 @@ class MoviesController < ApplicationController
 
   def show
     movie = Movie.find(params[:id])
-    render json: movie
+    render json: movie,  each_serializer: MovieSummarySerializer
   end
 
   private
---
+++ app/serializers/author_serializer.rb
@@ -0,0 +1,5 @@
+class AuthorSerializer < ActiveModel::Serializer
+  attributes :id, :name 
+  has_one :profile
+  has_many :posts, serializer: AuthorPostSerializer
+end
